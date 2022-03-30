8054f8d030b4a1b3758825e74602b1826cf4ee91
UmutAlihan <alihandikel@gmail.com>
2022-03-31 01:28:02 +0300

    first write test cases and mock objects before writing feature

--- src/hypermodern_python/wikipedia.py
+++ src/hypermodern_python/wikipedia.py
@@ -3,10 +3,12 @@ import requests
 import click
 
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 def random_page():
+    url = API_URL.format(language=language)
+
     try:
         with requests.get(API_URL) as response:
             response.raise_for_status()
